131bf7449ae9a83abb78d42d70388ed0ed399b3d
h352@my.centennialcollege.ca <harpreetkaur@MacBook-Pro-92.local>
2022-10-30 19:40:39 -0400

    intial commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+
+module.exports =
+{
+    //"URI" : "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://harpreet:YFBrjMwMcb7l1Iot@cluster0.dc5lf0b.mongodb.net/FavouriteBookList?retryWrites=true&w=majority",
+    
+}
